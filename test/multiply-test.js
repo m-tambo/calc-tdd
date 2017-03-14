8a6eca00ad3511b75a7621246f014fd7be6cce3e
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-14 11:43:48 -0500

    added tests

---
+++ test/multiply-test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { assert: { isNumber, equal }} = require('chai')
+const multiply = require('../lib/multiply.js')
+
+describe('multiply', () => {
+  it('should return a number', () => {
+    isNumber(multiply())
+  });
+
+  it('should return the product of two numbers', () => {
+    equal(multiply(10, 2), 20)
+  });
+
+})
---
+++ test/subtract-test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { assert: { isNumber, equal }} = require('chai')
+const subtract = require('../lib/subtract.js')
+
+describe('subtract', () => {
+  it('should return a number', () => {
+    isNumber(subtract())
+  });
+
+  it('should return the difference of two numbers', () => {
+    equal(subtract(10, 2), 8)
+  });
+
+})
